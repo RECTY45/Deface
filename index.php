aa7e41d80a53534312d7e5d6a61a3074b5773a1a
Fery Fadul Rahman <68836194+RECTY45@users.noreply.github.com>
2025-08-04 17:48:48 +0800

    Update and rename readme.php to index.php

---
+++ index.php
@@ -0,0 +1,449 @@
+<Doctype html>
+<html>
+<head>
+<link rel="icon" type="image/png" href="https://i.pinimg.com/originals/f4/35/22/f43522bf3b4bbe2ed9927c250467bd3b.jpg">
+<title>Pwneed By Why !!!!</title>
+</head>
+  <font face="trade Winds" size="5">
+  <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssKPgeGmFQGrgR1HXyfS%2fplvFJ1IJVqxDTXWbkKtCLHqMcNtOzzIAa3LYu%2fxmu%2fXYppcRv%2fBl%2beQhKvqffLr4HQmAdcSNuXg%2bvImaqSFMTTOo1ddoZdm4l51kVsxFr%2f3RkuM2qFv1fqD2MndvaUKQ0Vl1nCLH%2b9vWoyPmfvxvBkZj6tNnE13asv5aiVS%2fHCZM%2fF0Qw94TQ5DK5wldln1fRBR9zzvfrrSopeEB7RlW1iqXeAsy7u8ZekIgZo8xhT1F4%2b6bgK3P%2fENJoFVBIDGGwUqfqgdZC9SzzIZRDK3iaVXugSp8G%2f%2bXGdH5barZ7qXMMYvEouwDFTubyevYfm0PAPoPZsErwQk8JydlXDpTbl4jmghTQKvPYt26rJx6vVA8SEeRgYoXuvOyiMCAM3EF7WzaE6A4rPvCRTYDG16lV3wUoV8dITeC3tplZK4Iec%2f5htqMObrHn%2b1kiCa3San7wgBGuofJDXUDG27%2fxOAhE5CPkQ%2fzXpyJshEg%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+    <div id="foter" class="foter" color=#000000 style="position: fixed; top: 75px; left: -235px; width: 600px; padding: 10px; font-size: 16px; text-align: center; color: rgb(255, 255, 255); font-family: &#39;trebuchet ms&#39;, verdana, arial, sans-serif;transform: rotate(-45deg);transform-origin: 50% 0px;-o-transform: rotate(-45deg); -o-transform-origin: 50% 0px;-moz-transform: rotate(-45deg); -moz-transform-origin: 50% 0px; -webkit-transform: rotate(-45deg); -webkit-transform-origin: 50% 0px; background-color: rgb(0, 0, 0); border: 1px solid rgb(170, 170, 170); z-index: 9999; opacity: 0.5;"><b>
+    <font face="trade Winds" size="5">
+    <marquee direction="right" scrollamount="10" width="60%">Welcome Back!!!</marquee></font></b></div></span>
+</script>
+<br>
+	    <div id="jam" color="white" style="text-shadow: 0 0 0.2em red, 0 0 0.2em red;">
+       6:23:17
+      </div> 
+</style> 
+  <script type="text/javascript">
+// 1 detik = 1000
+window.setTimeout("waktu()",1000);  
+function waktu() {   
+var tanggal = new Date();  
+setTimeout("waktu()",1000);  
+document.getElementById("jam").innerHTML = tanggal.getHours()+":"+tanggal.getMinutes()+":"+tanggal.getSeconds();
+}
+</script>
+ </head> 
+ <body> 
+<div color="white" style="text-shadow: 0 0 0.2em white, 0 0 0.2em red;"><script type="text/javascript">
+  
+  var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+  var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth();
+  var thisDay = date.getDay(),
+  thisDay = myDays[thisDay];
+  var yy = date.getYear();
+  var year = (yy < 1000) ? yy + 1900 : yy;
+  document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+  </script> </font>
+<body>
+	
+<style> 
+body{ 
+background-image:url(https://media.tenor.com/zZ4jBBtkvdYAAAAd/chainsaw-man-chainsaw.gif); 
+background-size:cover; 
+ background-attachment: fixed; } 
+</style>
+<script src="https://cdn.rawgit.com/FicriPebriyana/efek/5ad2fdb2/daun-berguguran.js" type="text/javascript"></script>
+<br>
+ <font color="white" style="text-shadow: 0 0 0.2em red, 0 0 0.2em red;">______________________________________________________</font>
+     <h1><font color="white">INILAH</font><font color="red"> HIDUP !!!</h1>
+  
+ <br><img src="https://asset.kompas.com/crops/2RnkgZTEZDB1wrkTTVewExs-e24=/22x0:610x392/1200x800/data/photo/2023/10/25/6538cf6057235.png"  width="200px" style="border:2px solid red; border-radius:50%; box-shadow:2px 2px 15px red;"> 
+ <br>
+   <br>
+     <br>
+   <h1><font color="white" style="text-shadow: 0 0 0.2em red, 0 0 0.2em red;">Pwneed By Why</h1>
+  <font color="white" style="text-shadow: 0 0 0.2em red, 0 0 0.2em red;">______________________________________________________</font>
+  <br>
+ 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> 
+
+
+
+    <link href="https://fonts.googleapis.com/css?family=Bahiana|Black+Ops+One|Josefin+Slab |New+Rocker|Quicksand|Wallpoet|orbitron" rel="stylesheet">
+
+
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"> 
+
+<style>
+
+
+ body {
+
+
+
+    background-color:black;
+
+
+
+    text-align:center;
+
+
+
+	color: #fff;
+
+
+
+	font-family:Times;
+
+
+
+    position: center;
+
+
+
+    width: 800px;
+
+
+
+	font-size: 18px;
+
+
+
+    margin: 0 auto;
+
+
+
+    }
+    #myImg {
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+#myImg:hover {opacity: 0.7;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (Image) */
+.modal-content {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+}
+
+/* Caption of Modal Image (Image Text) - Same Width as the Image */
+#caption {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+  text-align: center;
+  color: #ccc;
+  padding: 10px 0;
+  height: 150px;
+}
+
+/* Add Animation - Zoom in the Modal */
+.modal-content, #caption {
+  animation-name: zoom;
+  animation-duration: 0.6s;
+}
+
+@keyframes zoom {
+  from {transform:scale(0)}
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+  .modal-content {
+    width: 100%;
+  }
+
+ .nick{
+
+
+
+       animation: blink 5s infinite;
+
+
+
+       font-family:Times;
+
+
+
+       font-size:18px;
+
+
+
+  }
+
+
+
+  @keyframes blink{
+
+
+
+         to{opacity: .0;}
+
+
+
+   }
+
+
+
+h2 {
+
+
+
+ font-family:Times;
+
+
+
+ color: #fff;
+
+
+
+ }
+
+
+
+ button {
+
+
+
+    color: #fff;
+
+
+
+    background: transparent linear-gradient(to right, #27c4f5, #a307ba, #fd8d32, #70c050, #27c4f5);
+
+
+
+	background-size: 500% 500%;
+
+
+
+	-webkit-animation: Gradient 15s ease infinite;
+
+
+
+	-moz-animation: Gradient 15s ease infinite;
+
+
+
+	animation: Gradient 15s ease infinite;
+
+
+
+}
+
+
+
+
+
+
+
+@-webkit-keyframes Gradient {
+
+
+
+	0% {
+
+
+
+		background-position: 0% 50%
+
+
+
+	}
+
+
+
+	50% {
+
+
+
+		background-position: 100% 50%
+
+
+
+	}
+
+
+
+	100% {
+
+
+
+		background-position: 0% 50%
+
+
+
+	}
+
+
+
+}
+
+
+
+
+
+
+
+@-moz-keyframes Gradient {
+
+
+
+	0% {
+
+
+
+		background-position: 0% 50%
+
+
+
+	}
+
+
+
+	50% {
+
+
+
+		background-position: 100% 50%
+
+
+
+	}
+
+
+
+	100% {
+
+
+
+		background-position: 0% 50%
+
+
+
+	}
+
+
+
+}
+
+
+
+@keyframes Gradient {
+
+
+
+	0% {
+
+
+
+		background-position: 0% 50%
+
+
+
+	}
+
+
+
+	50% {
+
+
+
+		background-position: 100% 50%
+
+
+
+	}
+
+
+
+	100% {
+
+
+
+		background-position: 0% 50%
+
+
+
+	}
+
+
+
+}
+
+
+
+}
+
+
+
+ 
+
+
+
+</style>
+<body>
+
+<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" ></iframe><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncX0AGAZ9l0NcarmYe9no1%2b3YJ4YvIpG56keu%2bg5bpahbAtUPj6O1Z9X3QL1tm0AyZ8xbbZXKyKfTd602G0u3hvctBhTsahRyDxKknjNZUFUM1bHPvN96ZRzX%2fKbZkjn5HX6WKy92m0lQoa0pvvxLMImPsp%2fabC9lWzOkLF2NhUu%2bzP%2bDZjMHDUtkP4vQC22Nz0TTSJkMvFmdGgEDQSDb09Gcd%2fOWC0pyaWIl8al2x8BhmxOyUazsOYOw%2bdjfY%2bmbR55yn10G1ly%2bDODLYyia5HqaQxaXiPu6%2bGqdT9G41r2LrbU4jKsyDvSAGrZ6C%2fTrcen8ZpG8%2bAyJAF6dr2WvRFUX6EAo3anQunBQ%2bgPU5Rpq8vTqjp5q%2b1UjW%2fCdnhJRFIsiiqzceT7NjauW%2fgBDWkpNlGbcDE91%2f%2bBwE%2bkS8Sci5Lyp%2bMA7vybep%2bFG0mJtVtEqx6z%2b9woitfkaSzrSGhN6TfUs2nydvBRQNAirq9Be8rfXpLTC7zfU7t%2fRikWdekmtsmG2CTe0b6vTYIQ0QsAC7s5ORzvbACKusYdH07QyHloG0J31U611kMMO84go%2bAfEIJEy4t7pbJ0mapCsJf5rNV8B60y6mr" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</div>
+  </body>
+ <br>
+<br>
+
+<audio id="audioNotifikasi" autoplay>
+  <source src="https://sndup.net/79cr/d" type="audio/mpeg">
+</audio>
+<button style="border:2px solid green; border-radius:30px; background-color:purple;" onclick="playAudio()" type="button"><font face="monaco" size="3" color="black">Play Music</font></button>
+<button style="border:2px solid green; border-radius:30px; background-color:purple;" onclick="pauseAudio()" type="button"><font face="monaco" size="3" color="black">Pause Music</font></button> <br><br>
+
+<script type="text/javascript">
+var el = document.getElementById("audioNotifikasi"); 
+
+function playAudio() { 
+  el.play(); 
+} 
+function pauseAudio() { 
+  el.pause(); 
+} 
+</script>
+<script>
+<center>
+  <div class="col-md-3"></div>
+  <div class="col-md-6 well">
+</body>
+ </html>
